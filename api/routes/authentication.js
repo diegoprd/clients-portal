a4f9c96bb8bb701aacea08ed7c7f086fa4f4e34d
Diego Prado <diegoprd@gmail.com>
2016-01-27 13:18:58 -0300

    Bring up the clients and main user structures

--- api/routes/authentication.js
+++ api/routes/authentication.js
@@ -1,88 +1,61 @@
-var _ = require('lodash');
-var mongoose = require('mongoose');
-var User = require('../models/user');
-var userValidator = require('../validators/userValidator');
-var emailValidator = require('../validators/emailValidator');
+var config = require('../config');
 
+
+var _authenticate = function(req, res, next) {
+
+  if (req.body.username !== req.body.passoword) {
+    return res.send(404, [{message:'Authentication failed, double check your credentials'}]);
+  }
+
+  if (req.body.username !== config.admin.userName) {
+    return res.send(404, [{message:'Authentication failed, double check your credentials'}]);
+  }
+
+  //Retriving full name for displaying purposes
+  res.json({ fullname: config.admin.displayName});
+
+};
 
 module.exports = function(app) {
 
   //Registers the new user (engineer or innovator) and signs it in.
-	app.post('/signup', function(req, res, next) {
-    if (req.body.username && req.body.password && req.body.fullname) {
-      //picking params
-      var userProperties = {
-        fullname: req.body.fullname,
-        username: req.body.username,
-        password: req.body.password,
-        type: req.body.type,
-        status: req.body.status
-      };
+	// app.post('/signup', function(req, res, next) {
+ //    if (req.body.username && req.body.password && req.body.fullname) {
+ //      //picking params
+ //      var userProperties = {
+ //        fullname: req.body.fullname,
+ //        username: req.body.username,
+ //        password: req.body.password,
+ //        type: req.body.type,
+ //        status: req.body.status
+ //      };
 
-      var validator = userValidator.validate(userProperties);
+ //      var validator = userValidator.validate(userProperties);
 
-      if (validator.valid) {
-        User.create(new User(userProperties), function(err, user) {
-          if (err) next(err);
+ //      if (validator.valid) {
+ //        User.create(new User(userProperties), function(err, user) {
+ //          if (err) next(err);
 
-          next();
-        });
-      } else {
-        console.log(validator.errors);
-        res.statusCode = 422;
-        res.send(validator.errors);
-      }
-    } else {
-      var message = 'All fields are mandatory to signup (username, password and fullname)';
-      return res.send(401, [{message: message}]);
-    }
-  },_authenticate);
+ //          next();
+ //        });
+ //      } else {
+ //        console.log(validator.errors);
+ //        res.statusCode = 422;
+ //        res.send(validator.errors);
+ //      }
+ //    } else {
+ //      var message = 'All fields are mandatory to signup (username, password and fullname)';
+ //      return res.send(401, [{message: message}]);
+ //    }
+ //  },_authenticate);
 
 	app.post('/signin', function(req, res, next) {
-    if (req.body.username && req.body.password) {
-    	//TODO validate req.body.username and req.body.password
-	    //if is invalid, return 401
-	    if (req.body.username.indexOf(' ') !== -1 || req.body.password.indexOf(' ') !== -1 ) {
-	      var message = 'Username and Password cannot contain spaces';
-	      res.send(401, [{message: message}]);
-	    }
 
-      var eValidator = emailValidator.validate(req.body.username);
-
-      if(eValidator.valid){
-        _authenticate(req,res,next);
-      } else {
-        res.send(422, eValidator.errors);
-      }
-
-    } else {
-			var message = 'Username and Password are both mandatory fields';
-	    res.send(401, [{message: message}]);
+    if (!req.body.username || !req.body.password) {
+      return res.send(401, [{message: 'Username and Password are both mandatory fields'}]);
     }
-  });
-
-};
-
-var _authenticate = function(req, res, next) {
-
-      //Validating user
-      User.findOne({username: req.body.username}, function(err, user) {
-
-        if (err) next(err);
-
-        if (_.isNull(user)){
-          return res.send(404, [{message:'Authentication failed, double check your credentials'}]);
-        }
-
-        //Validating credentials
-        if (user.password != req.body.password) {
-          res.statusCode = 401;
-          res.send([{message: 'Authentication failed, double check your credentials'}]);
-        }
-
-        //Retriving full name for displaying purposes
-        res.json({ fullname: user.fullname});
-
-      });
+    _authenticate(req,res,next);
 
   };
+
+};
